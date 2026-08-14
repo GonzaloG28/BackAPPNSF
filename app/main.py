730ac8f479358948f0c4df0f1c9a6e100c55abba
GonzaloG28 <gonzalogimenez280@gmail.com>
2026-08-13 23:18:11 -0400

    configuracion de permisos

--- app/main.py
+++ app/main.py
@@ -5,17 +5,24 @@ from fastapi.middleware.cors import CORSMiddleware
 from app.database import Base, engine
 import app.models
 
-from app.routers import imports, swimmers, attendance,attendance_v2, competitions, convocatorias,exports, auth, gym, performance
+from app.routers import imports, swimmers, attendance, attendance_v2, competitions, convocatorias, exports, auth, gym, performance
+
 app = FastAPI(title="SwimAI API", version="0.1.0")
 
 Base.metadata.create_all(bind=engine)
 
+# 1. Defines explícitamente quién puede conectarse a tu API
+origenes_permitidos = [
+    "http://localhost:8081",             # Para que puedas seguir probando en tu PC
+    "https://swimmobilensf.vercel.app"   # El enlace oficial para tus profesores
+]
 
+# 2. Configuras el middleware con esa lista
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], # Permite cualquier origen
+    allow_origins=origenes_permitidos, 
     allow_credentials=True,
-    allow_methods=["*"], # Permite POST, GET, OPTIONS, etc.
+    allow_methods=["*"], 
     allow_headers=["*"],
 )
 
@@ -29,6 +36,7 @@ app.include_router(exports.router)
 app.include_router(gym.exercises_router)
 app.include_router(performance.router)
 app.include_router(attendance_v2.router)
+
 @app.get("/")
 def root():
     return {"status": "SwimAI backend corriendo"}
